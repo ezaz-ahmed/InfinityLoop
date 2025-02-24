2c606e01487800dd620ba7ec4a052a47428ce719
Ezaz Ahmed <ezaz@grype.ca>
2025-02-24 19:18:53 +0600

    🔥 chore: remove all files for a fresh start
    Deleted the entire project to start fresh

--- src/meta-options/dto/update-meta-option.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateMetaOptionDto } from './create-meta-option.dto';
-
-export class UpdateMetaOptionDto extends PartialType(CreateMetaOptionDto) {}
